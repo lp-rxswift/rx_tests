c611c0749ae2335c75fbab1f97d570a635c6be97
Lucas <lucaspedrazoli@gmail.com>
2021-01-11 18:41:53 -0300

    setup and create amb test

--- TestingTests/TestingOperators.swift
+++ TestingTests/TestingOperators.swift
@@ -9,9 +9,37 @@ class TestingOperators : XCTestCase {
 
   override func setUp() {
     super.setUp()
+    scheduler = TestScheduler(initialClock: 0)
   }
 
   override func tearDown() {
+    scheduler.scheduleAt(1000) {
+      self.subscription.dispose()
+    }
+    scheduler = nil
     super.tearDown()
+  }
+
+  func testAmb() {
+    let observer = scheduler.createObserver(String.self)
+    let observableA = scheduler
+      .createHotObservable([.next(100, "a"),
+                            .next(200, "b"),
+                            .next(300, "c")])
+    let observableB = scheduler
+      .createHotObservable([.next(90, "1"),
+                            .next(200, "2"),
+                            .next(300, "3")])
+
+    let ambObservable = observableA.amb(observableB)
+
+    self.subscription = ambObservable.subscribe(observer)
+    scheduler.start()
+
+    let results = observer.events.compactMap {
+      $0.value.element
+    }
+
+    XCTAssertEqual(results, ["1", "2", "3"])
   }
 }
