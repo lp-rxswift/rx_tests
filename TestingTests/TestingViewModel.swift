e3fe40187c14d81561a4c0ae64ef6e25a44d067f
Lucas <lucaspedrazoli@gmail.com>
2021-01-12 14:24:48 -0300

    add color test

--- TestingTests/TestingViewModel.swift
+++ TestingTests/TestingViewModel.swift
@@ -19,5 +19,23 @@ class TestingViewModel : XCTestCase {
     let expect = expectation(description: #function)
     let expectedColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
     var result: UIColor!
+
+    viewModel.color.asObservable()
+      .skip(1)
+      .subscribe(onNext: {
+        result = $0
+        expect.fulfill()
+      })
+      .disposed(by: disposeBag)
+
+    viewModel.hexString.accept("#ff0000")
+
+    waitForExpectations(timeout: 1.0) { error in
+      guard error == nil else {
+        XCTFail(error!.localizedDescription)
+        return
+      }
+      XCTAssertEqual(expectedColor, result)
+    }
   }
 }
